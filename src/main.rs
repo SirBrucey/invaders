7f82322d7d85550487be3f1e4a91701f1f84e20e
Stuart Bruce <programming@brucey.me.uk>
2023-03-05 13:45:54 +0000

    Setup audio

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use std::error::Error;
 
+use rusty_audio::Audio;
+
 fn main() -> Result<(), Box<dyn Error>> {
-    return Ok(());
+    // Setup audio
+    let mut audio = Audio::new();
+
+    for filename in &["explode", "lose", "move", "pew", "startup", "win"] {
+        audio.add(filename, format!("audio/{filename}.wav"));
+    }
+
+    audio.play("startup");
+
+    // Cleanup
+    audio.wait();
+    Ok(())
 }
